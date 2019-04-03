15b0266a18664ae9db1bb8e38b678d1a587ebb69
Krittawat Suphankomut <kratuwu@gmail.com>
2019-04-03 22:35:06 +0700

    Fixed heigh Content and Footer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,21 +1,22 @@
-import React, { SFC } from 'react'
-import styled from 'styled-components';
+import React, { SFC } from "react";
+import styled from "styled-components";
 
 const ContentStyled = styled.div`
-    top: 60px;  
-    padding: 0px 24px;
-    @media (min-width: 700px) {
-        padding: 0px;
-    }
-`
+  top: 60px;
+  padding: 0px 24px;
+  @media (min-width: 700px) {
+    padding: 0px;
+  }
+  min-height: calc(100vh - 120px);
+`;
 
-const Content:SFC = ({children}) => {
+const Content: SFC = ({ children }) => {
   return (
     <ContentStyled>
-      <p>content</p>
+      <div>content</div>
       {children}
     </ContentStyled>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
+const FooterStyled = styled.footer`
+  height: 60px;
+  align-items: center;
+`;
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
-}
+  return <FooterStyled>Footer</FooterStyled>;
+};
 
-export default Footer
+export default Footer;
