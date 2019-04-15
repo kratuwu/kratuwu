3933f1beb57d6455a8aaead9deef1048e9f5e23f
Krittawat Suphankomut <kratuwu@gmail.com>
2019-04-15 19:49:27 +0700

    Remove padding

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -8,10 +8,6 @@ import { ContentRef } from "../../types";
 
 const ContentWrapper = styled.div`
   margin-top: 60px;
-  padding: 0px 24px;
-  @media (min-width: 700px) {
-    padding: 0px;
-  }
 `;
 
 type MainLayoutProps = {
