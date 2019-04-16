a0b6fe99be00dfeb10666ef7af4a765143c8a5f3
Krittawat Suphankomut <kratuwu@gmail.com>
2019-04-16 23:33:01 +0700

    Add about layout

--- src/components/contents/About.tsx
+++ src/components/contents/About.tsx
@@ -1,7 +1,24 @@
 import React, { forwardRef } from "react";
+import styled from "styled-components";
 
+const AboutWrapper = styled.div`
+  height: calc(100vh - 60px);
+  display: flex;
+  justify-content: center;
+`
+
+const AboutTitle = styled.h1`
+  font-size: 32px;
+  @media(min-width: 640px){
+    font-size: 64px;
+  }
+`
 const About = ({ forwardedRef }: any) => {
-  return <div ref={forwardedRef}>About</div>;
+  return (
+    <AboutWrapper ref={forwardedRef}>
+      <AboutTitle>About</AboutTitle>
+    </AboutWrapper>
+  )
 };
 
 export default forwardRef((props, ref) => <About {...props} forwardedRef={ref} />);
