1f749526355e22698bb7edaaca7d3ee73228e8b9
Krittawat Suphankomut <kratuwu@gmail.com>
2019-05-11 15:49:58 +0700

    Change to create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React, { createRef } from "react";
+import React, { createRef } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
 import MainLayout from "./components/layouts/MainLayout";
 import Home from "./components/contents/Home";
 import Work from "./components/contents/Work";
 import Contact from "./components/contents/Contact";
 import { ContentRef } from "../types";
 import { GlobalStyle } from "./styles";
-const App = () => {
+
+const App: React.FC = () => {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
   const homeRef = createRef();
   const workRef = createRef();
   const contactRef = createRef();
@@ -24,6 +46,6 @@ const App = () => {
       <Contact forwardedRef={contactRef} />
     </MainLayout>
   );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-// Your top level component
-import App from './App'
-
 library.add(fas, fab)
 
-// Export your top level component as JSX (for static rendering)
-export default App
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// Render your app
-if (typeof document !== 'undefined') {
-  const target = document.getElementById('root')
-
-  const renderMethod = target.hasChildNodes()
-    ? ReactDOM.hydrate
-    : ReactDOM.render
-
-  const render = (Comp: Function) => {
-    renderMethod(<Comp />, target)
-  }
-
-  // Render!
-  render(App)
-
-  // Hot Module Replacement
-  if (module && module.hot) {
-    module.hot.accept('./App', () => {
-      render(App)
-    })
-  }
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
