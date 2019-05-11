689e28aafb28b044d76bbf4582e735845b19eb0b
Krittawat Suphankomut <kratuwu@gmail.com>
2019-05-11 11:36:46 +0700

    Add  SideNavigation

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -6,31 +6,38 @@ import Navigation from "./Navigation";
 import Hamburger from "../Hamburger";
 import Footer from "./Footer";
 import { ContentRef } from "../../../types";
+// import SideNavigation from "./SideNavigation";
 
 const ContentWrapper = styled.div`
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  
+
 `;
 
-type MainLayoutProps = {
-  contentRefs: ContentRef[];
-};
 
 const Brand = styled.div`
   font-size: 24px;
   font-weight: bold;
   padding-left: 10px;
 `;
+type MainLayoutProps = {
+  contentRefs: ContentRef[];
+};
 
 const MainLayout: SFC<MainLayoutProps> = ({ children, contentRefs }) => {
   const [navOpen, toggleNav] = useState(false);
-
+  
   return(
     <div>
       <Header>
           <Brand>Kratuwu</Brand>
           <Hamburger onClick={()=>toggleNav(!navOpen)}/>
           <Navigation contentRefs={contentRefs} open = {navOpen}/>
+          {/* <SideNavigation/> */}
       </Header>
+      {/* <Hamburger onClick={()=>toggleNav(!navOpen)}/> */}
+      {/* <SideNavigation contentRefs={contentRefs} open = {navOpen}/> */}
+      {/* <SideNavigation contentRefs={contentRefs}/> */}
       <ContentWrapper children={children} />
       <Footer />
     </div>
