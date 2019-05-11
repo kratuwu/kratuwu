0f8c65ac736dd42a32f4fd2ccbce498ae6bdbd9f
Krittawat Suphankomut <kratuwu@gmail.com>
2019-05-11 19:27:11 +0700

    remove padding

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -4,11 +4,9 @@ import styled from "styled-components";
 
 const HamburgerWrapper = styled.div`
     padding-right: 10px;
-    /* margin-left: auto; */
     display: block;
     right: 0;
     position: fixed;
-    padding: 15px;
     cursor: pointer;
     @media (min-width: 768px) {
         display: none;
