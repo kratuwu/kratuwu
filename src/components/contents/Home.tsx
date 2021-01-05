9a99fea16f9ca2dab552590245c0116359917366
Krittawat Suphankomut <kratuwu@gmail.com>
2021-01-05 20:33:15 +0700

    change Caret style

--- src/components/contents/Home.tsx
+++ src/components/contents/Home.tsx
@@ -3,7 +3,7 @@ import styled, { keyframes } from "styled-components";
 import Social from "../Social";
 const HomeWrapper = styled.div`
   min-height: calc(100vh - 60px);
-  background-color: rgba(0, 0, 0, 0.65);
+  background-color: rgb(0, 0, 0);
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -16,19 +16,38 @@ const HomeWrapper = styled.div`
 `;
 
 const blinkCaret = keyframes`
-  from, to { color: transparent }
-  50% { color: white; }
+  from, to { background: transparent }
+  50% { 
+    background: linear-gradient(
+      60deg,
+      hsl(224, 85%, 66%),
+      hsl(269, 85%, 66%),
+      hsl(314, 85%, 66%),
+      hsl(359, 85%, 66%),
+      hsl(44, 85%, 66%),
+      hsl(89, 85%, 66%),
+      hsl(134, 85%, 66%),
+      hsl(179, 85%, 66%)
+    );
+    background-position: 100% 50%;
+  }
 `;
-const Caret = styled.span`
+const Caret = styled.div`
   animation: ${blinkCaret} 0.75s step-end infinite;
+  width: 3px;
+  margin-left: 3px;
 `;
 const TitleStyled = styled.h1`
   font-size: 32px;
   @media (min-width: 640px) {
     font-size: 64px;
   }
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
-const textTitle: string = "Welcome to Kratuwu.";
+
+const textTitle: string = "Welcome to Kratuwu";
 
 const Home = ({ forwardedRef }: any) => {
   const [textTyper, setTextTyper] = useState("");
@@ -49,8 +68,10 @@ const Home = ({ forwardedRef }: any) => {
   return (
     <HomeWrapper ref={forwardedRef}>
       <TitleStyled>
-        <span>{textTyper}</span>
-        <Caret>|</Caret>
+        <div>
+          <span>{textTyper}</span>
+        </div>
+        <Caret />
       </TitleStyled>
       <p>
         Hi! I'm Krittawat Suphankomut, I am a software developer from Thailand.
